648ee9faa1242453c8271a87ef7296f1fff6843c
dmden26 <dm-den26@yandex.ru>
2024-07-05 20:38:59 +1000

    MyFamily

---
+++ src/MyFamily.java
@@ -0,0 +1,8 @@
+public class MyFamily {
+    public static void main(String[] args) {
+        System.out.println("Дима");
+        System.out.println("Света");
+        System.out.println("Ксюша");
+        System.out.println("Руслан");
+    }
+}
